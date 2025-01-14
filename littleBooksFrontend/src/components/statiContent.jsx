cc0544e464c4d71882eacdded5e26699dbc51fbe
naylapalaoro <nayluclash@gmail.com>
2025-01-14 10:39:36 -0300

    logo carrito y ubicacion en el mapa

--- littleBooksFrontend/src/components/statiContent.jsx
+++ littleBooksFrontend/src/components/statiContent.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from 'react';
 import libros_cama from '../assets/libros_en_cama.jpeg';
 import libreria from '../assets/bibloteca_tenue.jpeg';
+import cart from '../assets/cart.png';
+import mapa from '../assets/mapa.jpg';
 import '../styles/home.css';
 
 //este componente es el estatico de la pagina, el unico cambio es la variocion de una imagen al mapa de ubicacion.
+
 const StatiContent = () => {
+
+    const [imagen,setImagen] = useState(libros_cama);
+
+    const verMapa = () => {
+        setImagen (imagen === libros_cama ? mapa : libros_cama);
+        
+    };
+    
+    const verCarrito = () => {
+
+    };
+
+
     return (
         <div className="content-container">
             <div className="content-block">
-                <img src={libros_cama} alt="Imagen de libros sobre una cama" className="content-image" />
+                <img src={imagen} alt="Imagen" className="content-image" />
                 <div className='context_text'>
                     <h2 >Tienda fisica</h2>
                     <h3 >1234 CONSTITUCION</h3>
                     <h3 >BUENOS AIRES, ARGENTINA</h3>
-                    <button className='location_button'>
+                    <button className='location_button' onClick={verMapa}>
                         Encuentranos
                     </button>
                 </div>
@@ -34,6 +50,11 @@ const StatiContent = () => {
                             <h5>Telefono</h5>
                             <h3>011-12345678</h3>
                         </div>
+                </div>
+                <div>
+                    <button type='button' onClick={verCarrito}>
+                            <img src={cart} alt="Carrito de compras" className='logoCarrito' />
+                    </button>
                 </div>
             </div>
         </div>
