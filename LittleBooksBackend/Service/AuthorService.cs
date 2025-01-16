17f1439e7dd688cfe77e50850a92b5df80c15cbc
Rixda <rixda.the.ice@gmail.com>
2025-01-16 07:54:14 -0300

    [example] structure Arthors

---
+++ LittleBooksBackend/Service/AuthorService.cs
@@ -0,0 +1,20 @@
+﻿using LittleBooksBackend.Interface;
+using LittleBooksBackend.Models;
+
+namespace LittleBooksBackend.Service
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly IAuthorRepository repository;
+
+        public AuthorService(IAuthorRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Author Test()
+        {
+            return repository.RepoTest();
+        }
+    }
+}
