63f12c8ffbb41f745a50b392baae4d6dc46efc18
naylapalaoro <nayluclash@gmail.com>
2025-01-20 15:44:47 -0300

    coneccion con la api, busqueda por genero

--- littleBooksFrontend/src/components/navbar.jsx
+++ littleBooksFrontend/src/components/navbar.jsx
@@ -3,10 +3,8 @@ import { useSearch } from '../hook/SearchContext';
 import '../styles/home.css';
 import { paletas } from '../utils/paletas';
 
-
 const Navbar = () => {
-
-  //cambia la paleta de colores de la pagina segun el genero de libro
+  // Cambia la paleta de colores según el género de libro
   const cambiarPaleta = (nombrePaleta) => {
     const paleta = paletas[nombrePaleta];
     for (const variable in paleta) {
@@ -14,76 +12,97 @@ const Navbar = () => {
     }
   };
 
-  //buscar segun autor o nombre de libro
   const [searchTerm, setSearchTerm] = useState(""); 
-  const { setResults } = useSearch(); 
+  const { setResults } = useSearch();
 
   const handleSearch = async (e) => {
-    e.preventDefault();
+  
+    if (!searchTerm) {
+      return;
+    }
 
     try {
       const response = await fetch(`https://pequenos-libros-publico.onrender.com/books/by-genre/${searchTerm}`);
 
-      if (!response.ok) { 
+      if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
       const data = await response.json();
-      setResults(data); 
-    } catch (error) {                 
+      setResults(data);
+    } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
   const handleButtonClick = (genre) => {
     cambiarPaleta(genre);
-    handleSearch(genre);
-    handleSearch({ preventDefault: () => {} });
+    setSearchTerm(genre); 
+    handleSearch({ preventDefault: () => {} }); 
   };
 
   return (
-      <nav className='navbar'>
-          <ul className='nav-list'>
-            <div>
-              <li className='Horror'><button type='button' onClick={()=>handleButtonClick("Horror")}>Terror</button></li>
-              <li className='Crime'><button type='button' onClick={()=>handleButtonClick("Crime")}>Crimen</button></li>
-            </div>  
-            <div>
-              <li className='Romance'><button type='button' onClick={()=>handleButtonClick("Romance")}>Romance</button></li>
-              <li className='ScienceFiction'><button type='button' onClick={()=>handleButtonClick("ScienceFiction")}>Ciencia Ficcion</button></li>
-            </div>
-            <div>  
-              <li className='Adventure'><button type='button' onClick={()=>handleButtonClick("Adventure")}>Aventura</button></li>
-              <li className='HistorcalFiction'><button type='button' onClick={()=>handleButtonClick("HistorcalFiction")}>Ficcion Historica</button></li>
-            </div>
-            <div> 
-              <li className='Mistery'><button type='button' onClick={()=>handleButtonClick("Mistery")}>Misterio</button></li>
-              <li className='Drama'><button type='button' onClick={()=>handleButtonClick("Drama")}>Drama</button></li>
-            </div>  
-            <div>
-              <li className='Fantasy'><button type='button' onClick={()=>handleButtonClick("Fantasy")}>Fantasia</button></li>
-              <li className='Philosophy'><button type='button' onClick={()=>handleButtonClick("Philosophy")}>Filosofia</button></li>
-            </div>
-            <div>    
-              <li className='MagicalRealism'><button type='button' onClick={()=>handleButtonClick("MagicalRealism")}>Realismo magico</button></li>
-            </div>   
-              <form onSubmit={handleSearch}>
-                <li>
-                  <input
-                    type="text"
-                    placeholder="Ingresa el titulo o el autor"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                  />
-                  <button type='submit' onClick={handleSearch}> Buscar</button>
-                </li>
-              </form>
-          </ul>
-      </nav>
-
+    <nav className="navbar">
+      <ul className="nav-list">
+        <div>
+          <li className="Horror">
+            <button type="button" onClick={() => handleButtonClick("Horror")}>Terror</button>
+          </li>
+          <li className="Crime">
+            <button type="button" onClick={() => handleButtonClick("Crime")}>Crimen</button>
+          </li>
+        </div>
+        <div>
+          <li className="Romance">
+            <button type="button" onClick={() => handleButtonClick("Romance")}>Romance</button>
+          </li>
+          <li className="ScienceFiction">
+            <button type="button" onClick={() => handleButtonClick("ScienceFiction")}>Ciencia Ficcion</button>
+          </li>
+        </div>
+        <div>
+          <li className="Adventure">
+            <button type="button" onClick={() => handleButtonClick("Adventure")}>Aventura</button>
+          </li>
+          <li className="HistorcalFiction">
+            <button type="button" onClick={() => handleButtonClick("HistorcalFiction")}>Ficción Histórica</button>
+          </li>
+        </div>
+        <div>
+          <li className="Mistery">
+            <button type="button" onClick={() => handleButtonClick("Mistery")}>Misterio</button>
+          </li>
+          <li className="Drama">
+            <button type="button" onClick={() => handleButtonClick("Drama")}>Drama</button>
+          </li>
+        </div>
+        <div>
+          <li className="Fantasy">
+            <button type="button" onClick={() => handleButtonClick("Fantasy")}>Fantasía</button>
+          </li>
+          <li className="Philosophy">
+            <button type="button" onClick={() => handleButtonClick("Philosophy")}>Filosofía</button>
+          </li>
+        </div>
+        <div>
+          <li className="MagicalRealism">
+            <button type="button" onClick={() => handleButtonClick("MagicalRealism")}>Realismo Mágico</button>
+          </li>
+        </div>
+        <form onSubmit={handleSearch}>
+          <li>
+            <input
+              type="text"
+              placeholder="Ingresa el título o el autor"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button type="submit">Buscar</button>
+          </li>
+        </form>
+      </ul>
+    </nav>
   );
 };
 
 export default Navbar;
-
-
